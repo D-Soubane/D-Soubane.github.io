9be5dd18400faf9b6b9329c10bc3955e645ce6fa
D-Soubane <140642149+D-Soubane@users.noreply.github.com>
2023-08-10 00:05:40 +0100

    Update check_access.php

--- check_access.php
+++ check_access.php
@@ -4,30 +4,31 @@ $password = $_POST['password'];
 $resource = $_POST['resource'];
 
 // Replace 'YOUR_ACTUAL_CNE' and 'YOUR_ACTUAL_PASSWORD' with the actual CNE and password.
-if ($cne === 'CNE' && $password === 'Let') {
+if ($cne === 'You' && $password === 'Let') {
     switch ($resource) {
         case 'physical_optics':
-            header('Location: physical_optics.html');
-            exit();
+            $file = 'physical_optics.pdf'; // Replace with the actual file name
             break;
         case 'electronics':
-            header('Location: electronics.html');
-            exit();
+            $file = 'electronics.pdf'; // Replace with the actual file name
             break;
         case 'electrodynamics':
-            header('Location: electrodynamics.html');
-            exit();
+            $file = 'electrodynamics.pdf'; // Replace with the actual file name
             break;
         case 'dielectric_properties':
-            header('Location: dielectric_properties.html');
-            exit();
+            $file = 'dielectric_properties.pdf'; // Replace with the actual file name
             break;
         // Add cases for other resources here
         default:
             echo 'Access granted but no resource specified.';
+            exit();
     }
+
+    // Set headers for file download
+    header('Content-Type: application/pdf'); // Set appropriate content type
+    header('Content-Disposition: attachment; filename="' . $file . '"');
+    readfile($file); // Serve the file for download
 } else {
     echo 'Access denied.';
 }
 ?>
-
