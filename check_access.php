92056db0324fb44f371faeaacea82d44521d223b
D-Soubane <140642149+D-Soubane@users.noreply.github.com>
2023-08-10 23:28:35 +0100

    Update check_access.php

--- check_access.php
+++ check_access.php
@@ -1,38 +1,34 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $cne = $_POST['cne'];
-    $password = $_POST['password'];
-    $resource = $_POST['resource'];
+$cne = $_POST['cne'];
+$password = $_POST['password'];
+$resource = $_POST['resource'];
 
-    // Replace 'YOUR_ACTUAL_CNE' and 'YOUR_ACTUAL_PASSWORD' with the actual CNE and password.
-    if ($cne === 'You' && $password === 'Let') {
-        switch ($resource) {
-            case 'physical_optics':
-                $file = 'physical_optics.pdf'; // Replace with the actual file name
-                break;
-            case 'electronics':
-                $file = 'electronics.pdf'; // Replace with the actual file name
-                break;
-            case 'electrodynamics':
-                $file = 'electrodynamics.pdf'; // Replace with the actual file name
-                break;
-            case 'dielectric_properties':
-                $file = 'dielectric_properties.pdf'; // Replace with the actual file name
-                break;
-            // Add cases for other resources here
-            default:
-                echo 'Access granted but no resource specified.';
-                exit();
-        }
-
-        // Set headers for file download
-        header('Content-Type: application/pdf'); // Set appropriate content type
-        header('Content-Disposition: attachment; filename="' . $file . '"');
-        readfile($file); // Serve the file for download
-    } else {
-        echo 'Access denied.';
+// Replace 'YOUR_ACTUAL_CNE' and 'YOUR_ACTUAL_PASSWORD' with the actual CNE and password.
+if ($cne === 'CNE' && $password === 'YourPassword') {
+    switch ($resource) {
+        case 'physical_optics':
+            $file = 'physical_optics.pdf'; // Replace with the actual file name
+            break;
+        case 'electronics':
+            $file = 'electronics.pdf'; // Replace with the actual file name
+            break;
+        case 'electrodynamics':
+            $file = 'electrodynamics.pdf'; // Replace with the actual file name
+            break;
+        case 'dielectric_properties':
+            $file = 'dielectric_properties.pdf'; // Replace with the actual file name
+            break;
+        // Add cases for other resources here
+        default:
+            echo 'Access granted but no resource specified.';
+            exit();
     }
+
+    // Set headers for file download
+    header('Content-Type: application/pdf'); // Set appropriate content type
+    header('Content-Disposition: attachment; filename="' . $file . '"');
+    readfile($file); // Serve the file for download
 } else {
-    echo 'Method not allowed.';
+    echo 'Access denied.';
 }
 ?>
